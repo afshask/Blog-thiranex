2806aa9f7c677b0faa4decd6c8ed9fc08822409e
Shaik Afsha <afshask83@gmail.com>
2026-08-11 22:53:46 +0530

    Created the basic file structure of blog platform

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const path = require("path");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect MongoDB
+connectDB();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Session
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Blog Platform Backend is Running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
